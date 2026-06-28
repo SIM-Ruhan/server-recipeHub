a708fb65edb7bf052ecb1bac2d6a0b910ba079f5
SIM Ruhan <simruhan22@gmail.com>
2026-06-29 02:20:43 +0600

    details page added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config()
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -47,7 +47,14 @@ const result = await cursor.toArray();
 res.send(result);
 })
 
-
+app.get("/api/recipes/:id", async (req,res) => {
+    const id = req.params.id;
+    const query = {
+        _id: new ObjectId(id)
+    }
+    const result = await recipeCollection.findOne(query);
+    res.send(result);
+})
 
 app.post("/api/recipes", async (req,res)=> {
     const recipe = req.body;
