9c0f3ddf28cebed5a966d75c035edd0160d09121
SIM Ruhan <simruhan22@gmail.com>
2026-07-02 08:35:17 +0600

    cores added

--- index.js
+++ index.js
@@ -85,10 +85,13 @@
 const express = require('express');
 const cors = require("cors");
 const app = express();
-const port = 8000;
+// const port = 8000;
 require("dotenv").config();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://recipe-hub-client-orcin.vercel.app', // Allow only your frontend
+  credentials: true
+}));
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
