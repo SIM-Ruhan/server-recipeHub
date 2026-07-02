60f1c180ea3bcdfdbdc10123ac55a7514634a9c8
SIM Ruhan <simruhan22@gmail.com>
2026-07-02 09:19:29 +0600

    cores added

--- index.js
+++ index.js
@@ -85,7 +85,7 @@
 const express = require('express');
 const cors = require("cors");
 const app = express();
-// const port = 8000;
+const port = 8000;
 require("dotenv").config();
 
 app.use(cors({
